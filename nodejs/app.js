149602d893ace8e185a1c68937f7d7a20b9c4819
mrrismawan <rifkirismawan@live.com>
2022-07-03 21:53:43 +0700

    Update Dependencies, invoke.js, and app.js

---
+++ nodejs/app.js
@@ -0,0 +1,76 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+var morgan = require('morgan')
+app.use(morgan('combined'))
+const bodyparser = require("body-parser");
+
+var cors = require('cors');
+const {createAloptama, createAlatoto, updateKondisiAloptama, updatePMCM} = require("./app/invoke");
+app.use(cors())
+app.use(bodyparser.json());
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000");
+})
+
+app.post("/createAloptama", async (req,res) => {
+    try {
+        let payload = {
+            "channelName": req.body.channelName,
+            "userId": req.body.userId,
+            "data": req.body.data
+        }
+
+        let result = await createAloptama(payload);
+        res.send(result)
+    } catch(error) {
+        res.status(500).send(error)
+    }   
+});
+
+app.post("/createAlatoto", async (req,res) => {
+    try {
+        let payload = {
+            "channelName": req.body.channelName,
+            "userId": req.body.userId,
+            "data": req.body.data
+        }
+
+        let result = await createAlatoto(payload);
+        res.send(result)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+});
+
+app.post("/updateKondisiAloptama", async (req,res) => {
+    try {
+        let payload = {
+            "channelName": req.body.channelName,
+            "userId": req.body.userId,
+            "data": req.body.data
+        }
+
+        let result = await updateKondisiAloptama(payload);
+        res.send(result)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+});
+
+app.post("/updatePMCM", async (req,res) => {
+    try {
+        let payload = {
+            "channelName": req.body.channelName,
+            "userId": req.body.userId,
+            "data": req.body.data
+        }
+
+        let result = await updatePMCM(payload);
+        res.send(result)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+});
--- nodejs/app/query.js
+++ nodejs/app/query.js
@@ -5,9 +5,10 @@ const yaml = require("js-yaml");
 const fs = require("fs");
 const path = require("path");
 const mspId = "Org2MSP";
-const CC_NAME = "fabcar";
+const CC_NAME = "monitoring";
 const CHANNEL = "mychannel";
 let ccp = null;
+
 async function invoke(user) {
     try {
         console.log("Invoking chaincode using : ", user);
